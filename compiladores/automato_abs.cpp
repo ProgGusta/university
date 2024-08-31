5b4bbb0adcf27f57af05084f9933aa4be33c2fa1
ProgGusta <gustavoprogpc@gmail.com>
2024-08-31 10:06:06 -0300

    Correcoes

--- compiladores/automato_abs.cpp
+++ compiladores/automato_abs.cpp
@@ -7,7 +7,8 @@
 
 using namespace std;
 
-struct AFD {
+struct AFD 
+{
     vector<string> estados;
     vector<char> alfabeto;
     map<pair<string, char>, string> transicoes;
@@ -15,7 +16,8 @@ struct AFD {
     set<string> estadosFinais;
     string cadeia;
 
-    bool processarCadeia() {
+    bool processarCadeia() 
+    {
         string estadoAtual = estadoInicial;
         for (char simbolo : cadeia) {
             pair<string, char> chave = make_pair(estadoAtual, simbolo);
@@ -27,72 +29,69 @@ struct AFD {
         return estadosFinais.find(estadoAtual) != estadosFinais.end();
     }
 
-    void mostrarAFD() const {
+    void mostrarAFD() const 
+    {
         cout << "Estados: ";
-        for (const string& estado : estados) {
+        for (const string& estado : estados)
             cout << estado << " ";
-        }
+        
         cout << endl;
 
         cout << "Alfabeto: ";
-        for (char simbolo : alfabeto) {
+        for (char simbolo : alfabeto)
             cout << simbolo << " ";
-        }
+
         cout << endl;
 
         cout << "Transicoes:" << endl;
-        for (const auto& transicao : transicoes) {
+        for (const auto& transicao : transicoes)
             cout << transicao.first.first << " -- " << transicao.first.second << " --> " << transicao.second << endl;
-        }
 
         cout << "Estado inicial: " << estadoInicial << endl;
 
         cout << "Estados finais: ";
-        for (const string& estadoFinal : estadosFinais) {
+        for (const string& estadoFinal : estadosFinais)
             cout << estadoFinal << " ";
-        }
+
         cout << endl;
     }
 };
 
-AFD lerArquivoEntrada(string nomeArquivo) {
+AFD lerArquivoEntrada(string nomeArquivo) 
+{
     AFD afd;
     ifstream arquivo(nomeArquivo);
     string linha;
 
     // Verifica se o arquivo foi aberto com sucesso
-    if (!arquivo.is_open()) {
+    if (!arquivo.is_open()) 
+    {
         cerr << "Erro ao abrir o arquivo!" << endl;
         return afd;
     }
 
     // Leitura dos estados
-    if (getline(arquivo, linha)) {
-        cout << linha << endl;
+    if (getline(arquivo, linha)) 
+    {
         istringstream ss(linha);
-        cout << ss.str() << endl << endl;
         string estado;
-        while (ss >> estado) {
+        while (ss >> estado)
             afd.estados.push_back(estado);
-        }
     }
 
     // Leitura do alfabeto
-    if (getline(arquivo, linha)) {
-        cout << linha << endl;
+    if (getline(arquivo, linha)) 
+    {
         istringstream ss(linha);
-        cout << ss.str() << endl << endl;
         char simbolo;
-        while (ss >> simbolo) {
+        while (ss >> simbolo)
             afd.alfabeto.push_back(simbolo);
-        }
     }
 
     // Leitura das transições
-    while (getline(arquivo, linha) && !linha.empty() && linha.find(' ') != string::npos) {
-        cout << linha << endl;
+    while (getline(arquivo, linha) && !linha.empty() && linha.find(' ') != string::npos) 
+    {
         istringstream ss(linha);
-        cout << ss.str() << endl << endl;
         string estadoOrigem, estadoDestino;
         char simbolo;
         ss >> estadoOrigem >> simbolo >> estadoDestino;
@@ -100,43 +99,37 @@ AFD lerArquivoEntrada(string nomeArquivo) {
     }
 
     // Leitura do estado inicial
-    if (!linha.empty()) {
-        cout << linha << endl << endl;
+    if (!linha.empty())
         afd.estadoInicial = linha;
-    }
 
     // Leitura dos estados finais
-    if (getline(arquivo, linha)) {
-        cout << linha << endl;
+    if (getline(arquivo, linha)) 
+    {
         istringstream ss(linha);
-        cout << ss.str() << endl << endl;
         string estadoFinal;
-        while (ss >> estadoFinal) {
+        while (ss >> estadoFinal)
             afd.estadosFinais.insert(estadoFinal);
-        }
     }
 
     // Leitura da cadeia
-    if (getline(arquivo, linha)) {
-        cout << linha << endl << endl;
+    if (getline(arquivo, linha))
         afd.cadeia = linha;
-    }
 
     return afd;
 }
 
-int main() {
+int main() 
+{
     AFD afd = lerArquivoEntrada("entrada.txt");
 
     // Mostrar os detalhes do AFD antes de processar a cadeia
     afd.mostrarAFD();
 
     // Processar a cadeia
-    if (afd.processarCadeia()) {
+    if (afd.processarCadeia())
         cout << "ACEITA" << endl;
-    } else {
+    else
         cout << "REJEITA" << endl;
-    }
 
     return 0;
 }
